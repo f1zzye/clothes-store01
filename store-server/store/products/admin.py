3121379cfdf30a6e0c81d68de692ae41eaa19cac
f1zzye <kosenko2401@gmail.com>
2024-02-15 23:44:29 +0200

    Initial commit

---
+++ store-server/store/products/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from products.models import Basket, Product, ProductCategory
+
+admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', 'image', ('price', 'quantity'), 'stripe_product_price_id', 'category')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity',)
+    extra = 0
+
+
+
